21f52c207cf9034a43411f48e059903b15e5a869
Erik Hollensbe <git@hollensbe.org>
2025-05-25 00:07:56 -0700

    move db models to own files
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

---
+++ src/db/models/session.rs
@@ -0,0 +1,71 @@
+use super::{User, DB};
+use anyhow::{anyhow, Result};
+use serde::{Deserialize, Serialize};
+use std::{collections::BTreeMap, ops::Deref};
+use validator::Validate;
+use welds::{state::DbState, WeldsModel};
+
+#[derive(
+    Debug,
+    Clone,
+    Eq,
+    PartialEq,
+    Ord,
+    PartialOrd,
+    WeldsModel,
+    Default,
+    Serialize,
+    Deserialize,
+    Validate,
+)]
+#[welds(table = "sessions")]
+#[welds(BelongsTo(user, User, "user_id"))]
+pub(crate) struct Session {
+    #[welds(primary_key)]
+    #[welds(rename = "session_id")]
+    pub id: u32,
+    pub expires: chrono::DateTime<chrono::Local>,
+    pub user_id: u32,
+}
+
+pub(crate) type JWTClaims = BTreeMap<String, String>;
+
+pub(crate) const JWT_SESSION_ID_KEY: &str = "kid";
+pub(crate) const JWT_EXPIRATION_TIME: &str = "exp";
+
+impl Session {
+    pub fn new_assigned(user: &User) -> DbState<Self> {
+        DbState::new_uncreated(Self {
+            user_id: user.id,
+            expires: chrono::Local::now()
+                .checked_add_signed(chrono::TimeDelta::days(7))
+                .unwrap(),
+            ..Default::default()
+        })
+    }
+
+    pub(crate) async fn from_jwt(db: &DB, claims: JWTClaims) -> Result<DbState<Self>> {
+        let session_id: u32 = claims[JWT_SESSION_ID_KEY].parse()?;
+        let list = Self::all()
+            .where_col(|c| c.id.equal(session_id))
+            .run(db.handle())
+            .await?;
+        let session = match list.first() {
+            Some(inner) => inner.deref(),
+            None => return Err(anyhow!("invalid session")),
+        };
+
+        let expires: chrono::DateTime<chrono::Local> = claims[JWT_EXPIRATION_TIME].parse()?;
+        if session.expires.signed_duration_since(expires).num_seconds() < 0 {
+            return Err(anyhow!("session is expired"));
+        }
+        Ok(DbState::db_loaded(session.clone()))
+    }
+
+    pub(crate) fn to_jwt(&self) -> JWTClaims {
+        let mut claims = JWTClaims::default();
+        claims.insert(JWT_SESSION_ID_KEY.into(), self.id.to_string());
+        claims.insert(JWT_EXPIRATION_TIME.into(), self.expires.to_rfc3339());
+        claims
+    }
+}
---
+++ src/db/models/user.rs
@@ -0,0 +1,77 @@
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
+
+use anyhow::{anyhow, Result};
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+use welds::WeldsModel;
+
+#[derive(
+    Debug,
+    Clone,
+    Eq,
+    PartialEq,
+    Ord,
+    PartialOrd,
+    WeldsModel,
+    Default,
+    Serialize,
+    Deserialize,
+    Validate,
+)]
+#[welds(table = "users")]
+#[welds(HasMany(sessions, super::Session, "user_id"))]
+pub(crate) struct User {
+    #[welds(primary_key)]
+    #[welds(rename = "user_id")]
+    #[serde(default = "u32::default")]
+    pub id: u32,
+
+    #[validate(length(min = 3, max = 30))]
+    pub username: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[validate(length(min = 3, max = 50))]
+    pub realname: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[validate(length(min = 6, max = 100), email)] // a@b.cd
+    pub email: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[validate(length(min = 10, max = 20))]
+    pub phone: Option<String>,
+
+    #[welds(ignore)]
+    // this should really skip totally, but is
+    // needed for tests.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "password")]
+    #[validate(length(min = 8, max = 100))]
+    pub plaintext_password: Option<String>,
+
+    #[serde(skip)]
+    pub(crate) password: String,
+}
+
+impl User {
+    pub(crate) fn login(&self, password: String) -> Result<()> {
+        let crypt = Argon2::default();
+        let parsed = PasswordHash::new(&self.password).map_err(|e| anyhow!(e.to_string()))?;
+        crypt
+            .verify_password(password.as_bytes(), &parsed)
+            .map_err(|e| anyhow!(e.to_string()))
+    }
+
+    pub(crate) fn set_password(&mut self, password: String) -> Result<()> {
+        let crypt = Argon2::default();
+        let salt = SaltString::generate(&mut OsRng);
+        self.password = crypt
+            .hash_password(password.as_bytes(), &salt)
+            .map_err(|e| anyhow!(e.to_string()))?
+            .to_string();
+        Ok(())
+    }
+}
